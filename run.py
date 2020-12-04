1b13e00630a092519d7ddc7f79f968bb892c2590
Lukas Elflein <lukaselflein@gmail.com>
2020-12-04 13:36:06 +0100

    more verbose example output

--- run.py
+++ run.py
@@ -14,17 +14,20 @@ nessus = ConnectionManager(**config)
 
 # Show the scans saved in the backend
 scans = nessus.list_scans()
-#print(scans)
+print(scans)
 
 # Show the xml of a scan
-example_xml_string = nessus.export_scan(8)
-# Show the xml of the latest scan
+# xml_string = nessus.export_scan(5)
+# print(xml_string)
+
+# Get the xml of the latest scan
 xml_string = nessus.export_latest()
 
 # Parse the scan
 scan = Scan(xml_string=xml_string)
 # Access the scan as a pandas DataFrame
 df = scan.df
+print(df[0:10])
 # Export the scan as a .csv table
 scan.to_csv(path="./example.csv")
 
